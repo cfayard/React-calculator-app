5ca1f2e9bb1865dd486802321813153962de12c6
Casey Fayard <fayardcasey@gmail.com>
2020-02-05 11:07:00 -0500

    pushing files to repo

---
+++ src/Calculator.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            num1: 0,
+            num2: 0,
+            result: 0
+            
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Best Calculator ever!</h1>
+                <input 
+                    type="number"
+                    value={this.state.num1}
+                    onChange={(event) => {
+                        this._updateNumber('num1', event.target.value)
+                    }}
+                />
+                <input type="number" 
+                    type="number"
+                    value={this.state.num2}
+                />
+                <br/>
+                <br/>
+                <button>+</button>
+                <button>-</button>
+                <button>/</button>
+                <button>*</button>
+                <br/>
+                <div></div>
+            </div>
+        )
+    }
+
+    _updateNumber = (key, newNumber) => {
+        this.setState({
+            [key]: newNumber
+        })
+    }
+}
+
+export default Calculator
